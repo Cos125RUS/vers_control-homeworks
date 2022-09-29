4749939cd272a938b1f202ddfe76c246057808f2
Cos125 <val250640@mail.ru>
2022-09-29 19:35:00 +1000

    Ex5

---
+++ Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter your name: ");
+string username = Console.ReadLine();
+
+if (username == "Val")
+{
+    Console.WriteLine("Pam-pam-pam");
+}
+else
+{
+    Console.Write("Hello ");
+    Console.WriteLine(username);
+}
